4ccc77b2e425893f47833df25cb1afb008d3ac52
jhgarciawillis <65470457+jhgarciawillis@users.noreply.github.com>
2024-07-28 15:09:32 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,37 +1,26 @@
+import streamlit as st
+from kucoin.client import Market, Trade, User
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 def initialize_clients():
     global market_client, trade_client, user_client
 
-    logger.debug("Initializing clients")
-
     try:
         API_KEY = st.secrets["api_credentials"]["api_key"]
-        logger.debug(f"API_KEY: {API_KEY}")
-
         API_SECRET = st.secrets["api_credentials"]["api_secret"]
-        logger.debug(f"API_SECRET: {API_SECRET}")
-
         API_PASSPHRASE = st.secrets["api_credentials"]["api_passphrase"]
-        logger.debug(f"API_PASSPHRASE: {API_PASSPHRASE}")
-
         API_URL = 'https://api.kucoin.com'
-        logger.debug(f"API_URL: {API_URL}")
 
-        logger.debug("Initializing Market client")
         market_client = Market(url=API_URL)
-        logger.debug("Market client initialized")
-
-        logger.debug("Initializing Trade client")
         trade_client = Trade(key=API_KEY, secret=API_SECRET, passphrase=API_PASSPHRASE, url=API_URL)
-        logger.debug("Trade client initialized")
-
-        logger.debug("Initializing User client")
         user_client = User(key=API_KEY, secret=API_SECRET, passphrase=API_PASSPHRASE, url=API_URL)
-        logger.debug("User client initialized")
-
         logger.info("Clients initialized successfully")
     except Exception as e:
         logger.error(f"Error initializing clients: {e}")
-        logger.exception("Exception traceback:")
         market_client = None
         trade_client = None
         user_client = None
