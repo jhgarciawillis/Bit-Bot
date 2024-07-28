db35baa28521f50c65b7ea0a4175442fced58c70
jhgarciawillis <65470457+jhgarciawillis@users.noreply.github.com>
2024-07-28 06:30:06 -0600

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -58,3 +58,41 @@ class Wallet:
             new_account = Account(account_type)
             new_account.add_currency(Currency(symbol, new_balance))
             self.add_account(new_account)
+
+    def simulate_market_buy(self, account_type, symbol, amount_usdt, price):
+        account = self.get_account_by_type(account_type)
+        if not account:
+            return False
+
+        usdt_balance = account.get_currency_balance('USDT')
+        if usdt_balance < amount_usdt:
+            return False
+
+        amount_crypto = amount_usdt / price
+        account.update_currency_balance('USDT', usdt_balance - amount_usdt)
+        current_crypto_balance = account.get_currency_balance(symbol)
+        account.update_currency_balance(symbol, current_crypto_balance + amount_crypto)
+        return True
+
+    def simulate_market_sell(self, account_type, symbol, amount_crypto, price):
+        account = self.get_account_by_type(account_type)
+        if not account:
+            return False
+
+        crypto_balance = account.get_currency_balance(symbol)
+        if crypto_balance < amount_crypto:
+            return False
+
+        amount_usdt = amount_crypto * price
+        account.update_currency_balance(symbol, crypto_balance - amount_crypto)
+        current_usdt_balance = account.get_currency_balance('USDT')
+        account.update_currency_balance('USDT', current_usdt_balance + amount_usdt)
+        return True
+
+    def get_account_summary(self):
+        summary = {}
+        for account in self.accounts:
+            summary[account.account_type] = {
+                currency.symbol: currency.balance for currency in account.currencies
+            }
+        return summary
