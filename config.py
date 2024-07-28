84e20e289a3b2d1fa7fc1d76b36510d2c0ca7c08
jhgarciawillis <65470457+jhgarciawillis@users.noreply.github.com>
2024-07-28 14:48:02 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,19 @@
 # config.py
-import streamlit as st
-from kucoin.client import Market, Trade, User
-
 def initialize_clients():
     global market_client, trade_client, user_client
 
-    API_KEY = st.secrets["API_KEY"]
-    API_SECRET = st.secrets["API_SECRET"]
-    API_PASSPHRASE = st.secrets["API_PASSPHRASE"]
+    API_KEY = st.secrets["api_credentials"]["api_key"]
+    API_SECRET = st.secrets["api_credentials"]["api_secret"]
+    API_PASSPHRASE = st.secrets["api_credentials"]["api_passphrase"]
     API_URL = 'https://api.kucoin.com'
 
-    market_client = Market(url=API_URL)
-    trade_client = Trade(key=API_KEY, secret=API_SECRET, passphrase=API_PASSPHRASE, url=API_URL)
-    user_client = User(key=API_KEY, secret=API_SECRET, passphrase=API_PASSPHRASE, url=API_URL)
+    try:
+        market_client = Market(url=API_URL)
+        trade_client = Trade(key=API_KEY, secret=API_SECRET, passphrase=API_PASSPHRASE, url=API_URL)
+        user_client = User(key=API_KEY, secret=API_SECRET, passphrase=API_PASSPHRASE, url=API_URL)
+        logger.info("Clients initialized successfully")
+    except Exception as e:
+        logger.error(f"Error initializing clients: {e}")
+        market_client = None
+        trade_client = None
+        user_client = None
