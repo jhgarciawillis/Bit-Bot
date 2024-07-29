2df7d60a9bde64a6fe88af7d53a2c0d005842a1c
jhgarciawillis <65470457+jhgarciawillis@users.noreply.github.com>
2024-07-29 00:42:02 -0600

    Update chart_utils.py

--- chart_utils.py
+++ chart_utils.py
@@ -1,4 +1,5 @@
 import plotly.graph_objects as go
+import plotly.subplots as sp
 import pandas as pd
 from trading_bot import TradingBot
 
@@ -6,43 +7,37 @@ class ChartCreator:
     def __init__(self, bot: TradingBot):
         self.bot = bot
 
-    def create_time_series_chart(self, chosen_symbols, chart_type):
-        fig = go.Figure()
-        
-        for symbol in chosen_symbols:
-            self.add_symbol_chart_traces(fig, symbol, chart_type)
-        
-        self.add_total_profits_chart(fig, chart_type)
-        
-        fig.update_layout(title=f'{chart_type} Over Time', xaxis_title='Time', yaxis_title='Value')
+    def create_charts(self):
+        fig = sp.make_subplots(rows=2, cols=2, subplot_titles=("Price", "Buy Prices", "Target Sell Prices", "Total Profits"))
+
+        self.add_price_chart(fig, 1, 1)
+        self.add_buy_prices_chart(fig, 1, 2)
+        self.add_target_sell_prices_chart(fig, 2, 1)
+        self.add_total_profits_chart(fig, 2, 2)
+
+        fig.update_layout(height=800, showlegend=False)
         return fig
 
-    def add_symbol_chart_traces(self, fig, symbol, chart_type):
-        history = self.bot.wallet.get_currency_history("trading", symbol.split('-')[0])
-        if history and history['price_history']:
-            df = pd.DataFrame(history['price_history'], columns=['timestamp', 'price'])
-            df.set_index('timestamp', inplace=True)
-            
-            if chart_type == 'Price':
-                fig.add_trace(go.Scatter(x=df.index, y=df['price'], mode='lines', name=f'{symbol} Price'))
-            
-            if chart_type in ['Buy Prices', 'Target Sell Prices']:
-                self.add_buy_sell_price_traces(fig, symbol, chart_type)
+    def add_price_chart(self, fig, row, col):
+        for symbol in self.bot.symbol_allocations:
+            history = self.bot.wallet.get_currency_history("trading", symbol.split('-')[0])
+            if history and history['price_history']:
+                df = pd.DataFrame(history['price_history'], columns=['timestamp', 'price'])
+                df.set_index('timestamp', inplace=True)
+                fig.add_trace(go.Scatter(x=df.index, y=df['price'], mode='lines', name=f'{symbol} Price'), row=row, col=col)
 
-    def add_buy_sell_price_traces(self, fig, symbol, chart_type):
+    def add_buy_prices_chart(self, fig, row, col):
         for status in self.bot.status_history:
             for trade in status['active_trades'].values():
-                if trade['symbol'] == symbol:
-                    if chart_type == 'Buy Prices':
-                        fig.add_trace(go.Scatter(x=[trade['buy_time']], y=[trade['buy_price']], 
-                                                 mode='markers', name=f'{symbol} Buy', marker_symbol='triangle-up'))
-                    else:
-                        fig.add_trace(go.Scatter(x=[trade['buy_time']], y=[trade['target_sell_price']], 
-                                                 mode='markers', name=f'{symbol} Target Sell', marker_symbol='triangle-down'))
+                fig.add_trace(go.Scatter(x=[trade['buy_time']], y=[trade['buy_price']], mode='markers', name=f"{trade['symbol']} Buy", marker_symbol='triangle-up'), row=row, col=col)
 
-    def add_total_profits_chart(self, fig, chart_type):
-        if chart_type == 'Total Profits':
-            profit_history = [(status['timestamp'], status['total_profit']) for status in self.bot.status_history]
-            df = pd.DataFrame(profit_history, columns=['timestamp', 'total_profit'])
-            df.set_index('timestamp', inplace=True)
-            fig.add_trace(go.Scatter(x=df.index, y=df['total_profit'], mode='lines', name='Total Profit'))
+    def add_target_sell_prices_chart(self, fig, row, col):
+        for status in self.bot.status_history:
+            for trade in status['active_trades'].values():
+                fig.add_trace(go.Scatter(x=[trade['buy_time']], y=[trade['target_sell_price']], mode='markers', name=f"{trade['symbol']} Target Sell", marker_symbol='triangle-down'), row=row, col=col)
+
+    def add_total_profits_chart(self, fig, row, col):
+        profit_history = [(status['timestamp'], status['total_profit']) for status in self.bot.status_history]
+        df = pd.DataFrame(profit_history, columns=['timestamp', 'total_profit'])
+        df.set_index('timestamp', inplace=True)
+        fig.add_trace(go.Scatter(x=df.index, y=df['total_profit'], mode='lines', name='Total Profit'), row=row, col=col)
